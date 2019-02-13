fddb340461c8e575df9e49ef6285193d9cbfc5ba
crepepepe <crepepepes@gmail.com>
2019-02-14 01:10:53 +0900

    add rpc dump_blocks

--- amo/util/rpc.go
+++ amo/util/rpc.go
@@ -2,7 +2,9 @@ package util
 
 import (
 	"encoding/json"
+	"fmt"
 	atypes "github.com/amolabs/amoabci/amo/types"
+	"github.com/tendermint/tendermint/libs/common"
 	"github.com/tendermint/tendermint/rpc/core"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 	"github.com/tendermint/tendermint/rpc/lib/server"
@@ -14,6 +16,7 @@ func addRoutes() {
 	routes := core.Routes
 	routes["transfer"] = rpcserver.NewRPCFunc(rpcTransfer, "from,to,amount")
 	routes["purchase"] = rpcserver.NewRPCFunc(rpcPurchase, "from,file_hash")
+	routes["dump_blocks"] = rpcserver.NewRPCFunc(rpcDumpBlocks, "start,size")
 }
 
 func rpcTransfer(from atypes.Address, to atypes.Address, amount *uint64) (*ctypes.ResultBroadcastTxCommit, error) {
@@ -46,4 +49,34 @@ func makeMessage(t string, payload interface{}) types.Tx {
 		panic(err)
 	}
 	return tx
+}
+
+type ResultDumpBlocks struct {
+	Blocks []ctypes.ResultBlock
+}
+
+func rpcDumpBlocks(start, size int64) (*ResultDumpBlocks, error) {
+	status, _ := core.Status()
+	lastHeight := status.SyncInfo.LatestBlockHeight
+	if start > lastHeight {
+		if start <= 0 {
+			return nil, fmt.Errorf("Height must be greater than 0")
+		}
+		if start > lastHeight {
+			return nil, fmt.Errorf("Height must be less than or equal to the current blockchain height")
+		}
+	}
+	length := common.MaxInt64(lastHeight-start, size)
+	result := ResultDumpBlocks{
+		Blocks: make([]ctypes.ResultBlock, length),
+	}
+	for i := int64(0); i < length; i++ {
+		height := int64(i+start)
+		block, err := core.Block(&height)
+		if err != nil {
+			return nil, err
+		}
+		result.Blocks[i] = *block
+	}
+	return &result, nil
 }
