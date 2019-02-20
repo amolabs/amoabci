f88f7decee7f2e3551e1bb411524ab366fcd74bf
Crepe <crepepepes@gmail.com>
2019-02-20 17:47:26 +0900

    add currency

---
+++ amo/types/currency.go
@@ -0,0 +1,16 @@
+package types
+
+import "encoding/binary"
+
+type Currency uint64
+
+func (c Currency) Serialize() ([]byte, error) {
+	b := make([]byte, 64/8)
+	binary.LittleEndian.PutUint64(b, uint64(c))
+	return b, nil
+}
+
+func (c *Currency) Deserialize(data []byte) error {
+	*c = Currency(binary.LittleEndian.Uint64(data))
+	return nil
+}
