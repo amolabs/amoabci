800b0d8196c59447d504b8c2ffea10a9ddbc6d66
Crepe <crepepepes@gmail.com>
2019-02-21 17:07:28 +0900

    Change data type of balance to Currency

--- amo/types/account.go
+++ amo/types/account.go
@@ -6,13 +6,13 @@ import (
 )
 
 type Account struct {
-	Balance        uint64  `json:"balance"`
+	Balance        Currency  `json:"balance"`
 	PurchasedFiles HashSet `json:"purchased_files"`
 }
 
 func (acc Account) Serialize() ([]byte, error) {
 	data := make([]byte, 64/8+len(acc.PurchasedFiles)*HashSize)
-	gobinary.PutUvarint(data, acc.Balance)
+	gobinary.PutUvarint(data, uint64(acc.Balance))
 	hset, _ := binary.Serialize(acc.PurchasedFiles)
 	copy(data[64/8:], hset)
 	return data, nil
@@ -20,7 +20,8 @@ func (acc Account) Serialize() ([]byte, error) {
 
 func (acc *Account) Deserialize(data []byte) error {
 	*acc = Account{}
-	acc.Balance, _ = gobinary.Uvarint(data)
+	balance, _ := gobinary.Uvarint(data)
+	acc.Balance = Currency(balance)
 	err := binary.Deserialize(data[64/8:], &acc.PurchasedFiles)
 	if err != nil {
 		panic(nil)
