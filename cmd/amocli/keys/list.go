06f9a21fb3917c93ec20682860d3ffb56d7fe434
Jaehong Ahn <monsieur.ahn@gmail.com>
2019-02-27 03:11:47 +0900

    Add a minor feature to sort private key list by its nickname

--- cmd/amocli/keys/list.go
+++ cmd/amocli/keys/list.go
@@ -2,6 +2,7 @@ package keys
 
 import (
 	"fmt"
+	"sort"
 )
 
 func List() error {
@@ -10,12 +11,20 @@ func List() error {
 		return err
 	}
 
+	sortKey := make([]string, 0, len(keyList))
+	for k := range keyList {
+		sortKey = append(sortKey, k)
+	}
+	sort.Strings(sortKey)
+
 	fmt.Printf("%-3s %-10s %-20s %-40s %-65s\n",
 		"seq", "nickname", "type", "address", "pubkey")
 
 	i := 0
-	for nickname, key := range keyList {
+	for _, nickname := range sortKey {
 		i += 1
+		key := keyList[nickname]
+
 		fmt.Printf("%-3d %-10s %-20s %-40x %-65x\n",
 			i, nickname, key.Type, key.Address, key.PubKey)
 	}
