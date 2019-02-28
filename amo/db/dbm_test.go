e090bd1436e4436cfd7187abb636cd53c1ceb8ff
crepepepe <crepepepes@gmail.com>
2019-02-28 17:15:17 +0900

    add test

---
+++ amo/db/dbm_test.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"github.com/amolabs/amoabci/amo/types"
+	"github.com/amolabs/tendermint-amo/crypto/p256"
+	cmn "github.com/amolabs/tendermint-amo/libs/common"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+const testRoot = "store_test"
+
+func setUp(t *testing.T) {
+	err := cmn.EnsureDir(testRoot, 0700)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func tearDown(t *testing.T) {
+	err := os.RemoveAll(testRoot)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestStore(t *testing.T) {
+	setUp(t)
+	s := NewStore(testRoot)
+	testAddr := p256.GenPrivKey().PubKey().Address()
+	balance := types.Currency(100)
+	s.SetBalance(testAddr, balance)
+	assert.Equal(t, balance, *s.GetBalance(testAddr))
+	tearDown(t)
+}
