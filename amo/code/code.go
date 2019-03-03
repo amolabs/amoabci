5aea052b9b929bf1c490445dc97890760da7f10a
crepepepe <crepepepes@gmail.com>
2019-03-03 17:42:33 +0900

    move code

---
+++ amo/code/code.go
@@ -0,0 +1,13 @@
+package code
+
+const (
+	TxCodeOK uint32 = iota
+	TxCodeBadParam
+	TxCodeNotEnoughBalance
+	TxCodeSelfTransaction
+	TxCodePermissionDenied
+	TxCodeTargetAlreadyBought
+	TxCodeTargetAlreadyExists
+	TxCodeTargetNotExists
+	TxCodeBadSignature
+)
