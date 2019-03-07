2782ba76f5fc2df3ff82d2252dbd0cb0fe4b8ec8
Yeon-Hyeong Yang <lbird94@gmail.com>
2019-03-07 16:39:53 +0900

    Add app.Query()
    - added balance query for "/balance" path
    - added response codes for abci_query

--- amo/amo_test.go
+++ amo/amo_test.go
@@ -2,6 +2,7 @@ package amo
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"testing"
 
 	abci "github.com/amolabs/tendermint-amo/abci/types"
@@ -9,6 +10,7 @@ import (
 	tdb "github.com/amolabs/tendermint-amo/libs/db"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/amolabs/amoabci/amo/code"
 	"github.com/amolabs/amoabci/amo/types"
 )
 
@@ -22,9 +24,66 @@ func TestInitChain(t *testing.T) {
 	assert.Equal(t, abci.ResponseInitChain{}, res)
 
 	// TODO: run series of app.Query() to check the genesis state
-	addrBytes, _ := hex.DecodeString("7CECB223B976F27D77B0E03E95602DABCC28D876")
-	addr := crypto.Address(addrBytes)
+	addrbin, _ := hex.DecodeString("7CECB223B976F27D77B0E03E95602DABCC28D876")
+	addr := crypto.Address(addrbin)
 	assert.Equal(t, types.Currency(100), app.store.GetBalance(addr))
 	//queryReq := abci.RequestQuery{}
 	//queryRes := app.Query(queryReq)
+}
+
+func TestQueryDefault(t *testing.T) {
+	db := tdb.NewMemDB()
+	app := NewAMOApplication(db)
+
+	// query
+	req := abci.RequestQuery{}
+	req.Path = "/nostore"
+	res := app.Query(req)
+	assert.Equal(t, code.QueryCodeBadPath, res.Code)
+}
+
+func TestQueryBalance(t *testing.T) {
+	db := tdb.NewMemDB()
+	app := NewAMOApplication(db)
+
+	// populate db store
+	addrbin, _ := hex.DecodeString("7CECB223B976F27D77B0E03E95602DABCC28D876")
+	addr := crypto.Address(addrbin)
+	queryjson, _ := json.Marshal(addr)
+	app.store.SetBalance(addr, types.Currency(100))
+
+	_addrbin, _ := hex.DecodeString("FFECB223B976F27D77B0E03E95602DABCC28D876")
+	_addr := crypto.Address(_addrbin)
+	_queryjson, _ := json.Marshal(_addr)
+
+	var req abci.RequestQuery
+	var res abci.ResponseQuery
+	var jsonstr []byte
+
+	// errors
+	req = abci.RequestQuery{Path: "/balance"}
+	res = app.Query(req)
+	assert.Equal(t, code.QueryCodeNoKey, res.Code)
+
+	req = abci.RequestQuery{Path: "/balance", Data: []byte("f8das")}
+	res = app.Query(req)
+	assert.Equal(t, code.QueryCodeBadKey, res.Code)
+
+	// XXX: current implementation returns zero balance for unknown address
+	req = abci.RequestQuery{Path: "/balance", Data: []byte(_queryjson)}
+	res = app.Query(req)
+	assert.Equal(t, code.QueryCodeOK, res.Code)
+	jsonstr, _ = json.Marshal(types.Currency(0))
+	assert.Equal(t, []byte(jsonstr), res.Value)
+	assert.Equal(t, req.Data, res.Key)
+	assert.Equal(t, string(jsonstr), res.Log)
+
+	// query
+	req = abci.RequestQuery{Path: "/balance", Data: []byte(queryjson)}
+	res = app.Query(req)
+	assert.Equal(t, code.QueryCodeOK, res.Code)
+	jsonstr, _ = json.Marshal(types.Currency(100))
+	assert.Equal(t, []byte(jsonstr), res.Value)
+	assert.Equal(t, req.Data, res.Key)
+	assert.Equal(t, string(jsonstr), res.Log)
 }
