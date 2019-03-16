c92e0d6df44e17b46fb084974cd4e6e6af2de762
crepepepe <crepepepes@gmail.com>
2019-03-16 23:53:01 +0900

    change Currency to uint256

--- amo/types/currency.go
+++ amo/types/currency.go
@@ -1,29 +1,81 @@
 package types
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
-	"strconv"
+	"math/big"
+
+	cmn "github.com/tendermint/tendermint/libs/common"
 )
 
-type Currency uint64
+const (
+	maxCurrencyHex    = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
+	currencyLen       = 256 / 8
+)
+
+// Currency uses big endian for compatibility to big.Int
+type Currency [currencyLen]byte
+
+var (
+	maxCurrency big.Int
+)
+
+func init() {
+	maxCurrency.SetString(maxCurrencyHex, 16)
+}
+
+func isExceed(i *big.Int) bool {
+	return maxCurrency.Cmp(i) != 1
+}
+
+func (c *Currency) Set(x uint64) *Currency {
+	binary.BigEndian.PutUint64(c[currencyLen-8:], x)
+	return c
+}
+
+func (c *Currency) SetString(x string, base int) (*Currency, error) {
+	i, ok := new(big.Int).SetString(x, base)
+	if !ok {
+		return nil, cmn.NewError("Fail to convert hex string(%v)", x)
+	}
+	if isExceed(i) {
+		return nil, cmn.NewError("Currency supports up to 32 bytes;%v", x)
+	}
+	b := i.Bytes()
+	*c = Currency{}
+	for i := 0; i < len(b); i++ {
+		c[currencyLen-i-1] = b[len(b)-i-1]
+	}
+	return c, nil
+}
+
+func (c Currency) String() string {
+	return new(big.Int).SetBytes(c[:]).Text(10)
+}
 
 func (c Currency) Serialize() ([]byte, error) {
-	b := make([]byte, 64/8)
-	binary.LittleEndian.PutUint64(b, uint64(c))
-	return b, nil
+	var buf bytes.Buffer
+	i := 0
+	for ; i < len(c); i++ {
+		if c[i] != 0 {
+			break
+		}
+	}
+	buf.WriteByte(byte(currencyLen-i))
+	buf.Write(c[i:])
+	return buf.Bytes(), nil
 }
 
 func (c *Currency) Deserialize(data []byte) error {
-	*c = Currency(binary.LittleEndian.Uint64(data))
+	for i := 0; i < len(data[1:]); i++ {
+		c[currencyLen-data[0]+byte(i)] = data[i+1]
+	}
 	return nil
 }
 
 func (c Currency) MarshalJSON() ([]byte, error) {
-	// TODO: big number
-	s := strconv.FormatUint(uint64(c), 10)
-	data := []byte("\"" + s + "\"")
-	return data, nil
+	return []byte("\"" + c.String() + "\""), nil
 }
 
 func (c *Currency) UnmarshalJSON(data []byte) error {
@@ -32,17 +84,54 @@ func (c *Currency) UnmarshalJSON(data []byte) error {
 		return errors.New(
 			"Currency should be represented as double-quoted string.")
 	}
+	*c = Currency{}
 	s = s[1 : len(s)-1]
+	if len(s) > currencyLen*2 {
+		return cmn.NewError("Currency supports up to 32 bytes(%v)", s)
+	}
 	if len(s) == 0 {
-		*c = Currency(0)
 		return nil
 	}
+	_, err := c.SetString(s, 10)
+	return err
+}
 
-	// TODO: big number
-	n, err := strconv.ParseUint(s, 10, 64)
-	if err != nil {
-		return err
+func (c *Currency) Add(a *Currency) *Currency {
+	x, y := new(big.Int).SetBytes(c[:]), new(big.Int).SetBytes(a[:])
+	if isExceed(x.Add(x, y)) {
+		panic(cmn.NewError("Cannot add"))
 	}
-	*c = Currency(n)
-	return nil
+	c.Set(0)
+	b := x.Bytes()
+	for i := 0; i < len(b); i++ {
+		c[currencyLen-i-1] = b[len(b)-i-1]
+	}
+	return c
+}
+
+func (c *Currency) Sub(a *Currency) *Currency {
+	x, y := new(big.Int).SetBytes(c[:]), new(big.Int).SetBytes(a[:])
+	if x.Sub(x, y).Sign() == -1 {
+		panic(cmn.NewError("Cannot subtract"))
+	}
+	c.Set(0)
+	b := x.Bytes()
+	for i := 0; i < len(b); i++ {
+		c[currencyLen-i-1] = b[len(b)-i-1]
+	}
+	return c
+}
+
+func (c Currency) Equals(a *Currency) bool {
+	return bytes.Equal(c[:], a[:])
+}
+
+func (c Currency) GreaterThan(a *Currency) bool {
+	x, y := new(big.Int).SetBytes(c[:]), new(big.Int).SetBytes(a[:])
+	return x.Cmp(y) == 1
+}
+
+func (c Currency) LessThan(a *Currency) bool {
+	x, y := new(big.Int).SetBytes(c[:]), new(big.Int).SetBytes(a[:])
+	return x.Cmp(y) == -1
 }
--- amo/types/currency_test.go
+++ amo/types/currency_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestCurrencyJSON(t *testing.T) {
-	amount := Currency(100)
+	amount := new(Currency).Set(100)
 	d, err := json.Marshal(amount)
 	assert.NoError(t, err)
 	assert.Equal(t, []byte("\"100\""), d)
@@ -26,9 +26,31 @@ func TestCurrencyJSON(t *testing.T) {
 
 	err = json.Unmarshal([]byte("\"\""), &amount)
 	assert.NoError(t, err)
-	assert.Equal(t, Currency(0), amount)
+	assert.Equal(t, new(Currency).Set(0), amount)
 
 	err = json.Unmarshal([]byte("\"100\""), &amount)
 	assert.NoError(t, err)
-	assert.Equal(t, Currency(100), amount)
+	assert.Equal(t, new(Currency).Set(100), amount)
+
+	err = json.Unmarshal([]byte("\"12QQ\""), &amount)
+	assert.Error(t, err)
+}
+
+func TestCurrencyAdd(t *testing.T) {
+	x := new(Currency).Set(1000)
+	y := new(Currency).Set(2000)
+	z := new(Currency).Set(3000)
+
+	// x += 2000
+	x.Add(y)
+	assert.Equal(t, z, x)
+	assert.True(t, x.Equals(z))
+	// x = 2000
+	x.Set(1000)
+	z.Sub(y)
+	assert.Equal(t, x, z)
+	// x < z
+	assert.True(t, x.LessThan(y))
+	// x > z
+	assert.False(t, x.GreaterThan(y))
 }
