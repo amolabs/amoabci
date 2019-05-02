5019a577b28dbbd13cc0242526a3973e365f8664
Yeon-Hyeong Yang <lbird94@gmail.com>
2019-05-02 18:24:41 +0900

    Fix error in retract operation

--- amo/operation/operation_test.go
+++ amo/operation/operation_test.go
@@ -352,9 +352,15 @@ func TestValidRetract(t *testing.T) {
 		Amount: *new(types.Currency).Set(400),
 	}
 	assert.Equal(t, code.TxCodeOK, op.Check(s, bob.addr))
-	resCode := op.Execute(s, bob.addr)
-	assert.Equal(t, code.TxCodeOK, resCode)
+	assert.Equal(t, code.TxCodeOK, op.Execute(s, bob.addr))
 	assert.Equal(t, new(types.Currency).Set(100), &s.GetDelegate(bob.addr).Amount)
+
+	op = Retract{
+		Amount: *new(types.Currency).Set(100),
+	}
+	assert.Equal(t, code.TxCodeOK, op.Check(s, bob.addr))
+	assert.Equal(t, code.TxCodeOK, op.Execute(s, bob.addr))
+	assert.Equal(t, zero, &s.GetDelegate(bob.addr).Amount)
 }
 
 func TestNonValidRetract(t *testing.T) {
