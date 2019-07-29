1c5b5a4c89df79caeffdef7c171e2eb23649bb17
Yeon-Hyeong Yang <lbird94@gmail.com>
2019-07-29 17:29:42 +0900

    Include encrypted keys as env

--- test_script/env.sh
+++ test_script/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-eval $(amocli key list | awk '{ if ($3 == "x" && $2 != "t0") printf "%s=%s\n",$2,$4 }')
+eval $(amocli key list | awk '{ if ($2 != "t0") printf "%s=%s\n",$2,$4 }')
 
 # Be careful about this. This account's balance is the source of all assets.
 t0=FD037CADE0A0B3C8FB5039BAC17779E9F6E8BD8F
