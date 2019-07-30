84d86774315d0f7b8252dc86471a9e8b04609796
Yeon-Hyeong Yang <lbird94@gmail.com>
2019-07-30 17:14:17 +0900

    Refine local test environment
    - use `persistent_peers` instead of `seeds`
    - find out external ip address and use it as `external_address`

--- DOCKER/run.sh
+++ DOCKER/run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-echo "set moniker =" $MONIKER
-echo "set seeds =" $SEEDS
+echo "set moniker = $MONIKER"
+echo "set peers = $PEERS"
+
+iface=$(route -n | grep '^0.0.0.0' | awk '{print $8}')
+extaddr=$(ip -f inet a show dev $iface | grep '\<inet\>' | head -1 | awk '{print $2}' | awk -F'/' '{print $1}')
+echo "set extaddr = $extaddr"
 
 cp config.toml.in config.toml
 sed -e s/@moniker@/$MONIKER/ -i.tmp config.toml
-sed -e s/@seeds@/$SEEDS/ -i.tmp config.toml
+sed -e s/@peers@/$PEERS/ -i.tmp config.toml
+sed -e s/@external@/tcp:\\/\\/$extaddr:26656/ -i.tmp config.toml
 mkdir config
 mkdir data
 mv -f config.toml config/
