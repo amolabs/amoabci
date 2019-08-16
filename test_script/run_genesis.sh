f66bc26edc2c94505a08e0427b7008f5ead24cd3
AHN Jaehong <monsieur.ahn@gmail.com>
2019-08-16 17:53:45 +0900

    Modify containers to communicate to each other

--- test_script/run_genesis.sh
+++ test_script/run_genesis.sh
@@ -7,7 +7,7 @@ docker-compose up -d val1
 sleep 2s
 
 # get val1's tendermint node addr
-val1addr=$(docker exec -it val1 tendermint show_node_id | tr -d '\015')
+val1addr=$(docker exec -it val1 tendermint show_node_id | tr -d '\015' | tr -d '^@')
 
 # update seed node's peer set with val1addr on docker-compose.yml 
 sed -e s/__val1_addr__/$val1addr/ -i.tmp docker-compose.yml
