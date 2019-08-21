77b4657a671b717e974fbbca62648dcc3b72ac69
Yeon-Hyeong Yang <lbird94@gmail.com>
2019-08-21 12:50:49 +0900

    Use named volume to make the data directory persistent

--- test_script/run_genesis.sh
+++ test_script/run_genesis.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 docker-compose up --no-start val1
-docker-compose run val1 mkdir -p /tendermint/config
-docker-compose run val1 mkdir -p /tendermint/data
+docker-compose run --rm val1 mkdir -p /tendermint/config
+docker-compose run --rm val1 mkdir -p /tendermint/data
 
-WD=$(dirname $0 | tr -d '\n')
+WD=$(dirname $0)
 docker cp $WD/priv_validator_key.json val1:/tendermint/config/
 docker cp $WD/priv_validator_state.json val1:/tendermint/data/
 
