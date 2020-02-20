3af4c6b384a2131a4f7b8029d8e4a9280751dc6e
Jaehong Ahn <monsieur.ahn@gmail.com>
2020-02-20 15:05:37 +0900

    Integrate TM into AMO
    - make AMO run with TM simultaneously
    - import TM commands to amod as submodules
    - make it possible for users to give custom home dir path for TM, AMO

--- cmd/amod/cmd/run.go
+++ cmd/amod/cmd/run.go
@@ -2,12 +2,20 @@ package cmd
 
 import (
 	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
 
 	"github.com/spf13/cobra"
-	"github.com/tendermint/tendermint/abci/server"
-	cmn "github.com/tendermint/tendermint/libs/common"
+	"github.com/spf13/viper"
+	abci "github.com/tendermint/tendermint/abci/types"
+	cfg "github.com/tendermint/tendermint/config"
+	tmflags "github.com/tendermint/tendermint/libs/cli/flags"
 	"github.com/tendermint/tendermint/libs/log"
+	nm "github.com/tendermint/tendermint/node"
+	"github.com/tendermint/tendermint/p2p"
+	"github.com/tendermint/tendermint/privval"
+	"github.com/tendermint/tendermint/proxy"
 
 	"github.com/amolabs/amoabci/amo"
 	"github.com/amolabs/amoabci/amo/store"
@@ -18,51 +26,46 @@ import (
  * amod |- run
  */
 
-var (
-	defaultAMODir = ".amo"
-
-	defaultDataDir = "data"
-
-	defaultMerkleDB       = "merkle"
-	defaultIndexDB        = "index"
-	defaultIncentiveDB    = "incentive"
-	defaultGroupCounterDB = "group_counter"
-
-	defaultStateFile = "state.json"
-
-	defaultAMODirPath = filepath.Join(os.ExpandEnv("$HOME"), defaultAMODir) // $HOME/.amo/
-)
-
-var runCmd = &cobra.Command{
+var RunCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Execute the daemon",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		amoDirPath, err := cmd.Flags().GetString("home")
+		amoDirPath, err := cmd.Flags().GetString("amo-home")
 		if err != nil {
 			return err
 		}
 
-		err = initApp(amoDirPath)
+		node, err := initApp(amoDirPath)
 		if err != nil {
 			return err
 		}
+
+		node.Start()
+		defer func() {
+			node.Stop()
+			node.Wait()
+		}()
+
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		<-c
+		os.Exit(0)
+
 		return nil
 	},
 }
 
-func initApp(amoDirPath string) error {
-	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
-	appLogger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
-
+func initApp(amoDirPath string) (*nm.Node, error) {
+	// amo dir
 	if _, err := os.Stat(amoDirPath); os.IsNotExist(err) {
 		os.Mkdir(amoDirPath, os.FileMode(0700))
 	}
 
+	// state file
 	stateFilePath := filepath.Join(amoDirPath, defaultStateFile)
-
 	stateFile, err := os.OpenFile(stateFilePath, os.O_CREATE, os.FileMode(0644))
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// TODO: do not use hard-coded value. use value from configuration.
@@ -70,48 +73,89 @@ func initApp(amoDirPath string) error {
 
 	merkleDB, err := store.NewDBProxy(defaultMerkleDB, dataDirPath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	indexDB, err := store.NewDBProxy(defaultIndexDB, dataDirPath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	incentiveDB, err := store.NewDBProxy(defaultIncentiveDB, dataDirPath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	groupCounterDB, err := store.NewDBProxy(defaultGroupCounterDB, dataDirPath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
+	// logger
+	appLogger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
+
+	// create app
 	app := amo.NewAMOApp(
 		stateFile,
 		merkleDB, indexDB, incentiveDB, groupCounterDB,
 		appLogger.With("module", "abci-app"),
 	)
 
-	srv, err := server.NewServer("tcp://0.0.0.0:26658", "socket", app)
+	node, err := newTM(app)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	srv.SetLogger(logger.With("module", "abci-server"))
-	if err := srv.Start(); err != nil {
-		return err
-	}
-	cmn.TrapSignal(
-		log.NewTMLogger(log.NewSyncWriter(os.Stdout)).With("module", "abci-server"),
-		func() {
-			// Cleanup
-			srv.Stop()
-		})
 
-	select {}
+	return node, nil
+}
+
+func newTM(app abci.Application) (*nm.Node, error) {
+	// parse config
+	config := cfg.DefaultConfig()
+	err := viper.Unmarshal(config)
+	if err != nil {
+		return nil, err
+	}
+	config.SetRoot(config.RootDir)
+	cfg.EnsureRoot(config.RootDir)
+	err = config.ValidateBasic()
+	if err != nil {
+		return nil, err
+	}
+
+	// logger
+	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
+	logger, err = tmflags.ParseLogLevel(config.LogLevel, logger, cfg.DefaultLogLevel())
+	if err != nil {
+		return nil, err
+	}
+
+	// read private validator
+	pv := privval.LoadFilePV(
+		config.PrivValidatorKeyFile(),
+		config.PrivValidatorStateFile(),
+	)
+
+	// read node key
+	nodeKey, err := p2p.LoadNodeKey(config.NodeKeyFile())
+	if err != nil {
+		return nil, err
+	}
+
+	// create node
+	return nm.NewNode(
+		config,
+		pv,
+		nodeKey,
+		proxy.NewLocalClientCreator(app),
+		nm.DefaultGenesisDocProviderFunc(config),
+		nm.DefaultDBProvider,
+		nm.DefaultMetricsProvider(config.Instrumentation),
+		logger,
+	)
 }
 
 func init() {
 	// init here
+	RunCmd.Flags().String("amo-home", defaultAMODirPath, "AMO home directory")
 }
--- cmd/amod/main.go
+++ cmd/amod/main.go
@@ -1,14 +1,50 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/amolabs/amoabci/cmd/amod/cmd"
+	"github.com/spf13/cobra"
+	tm "github.com/tendermint/tendermint/cmd/tendermint/commands"
+	cfg "github.com/tendermint/tendermint/config"
+	"github.com/tendermint/tendermint/libs/cli"
 )
 
 /* Commands (expected hierarchy)
  *
- * amod |- run 
+ * amod |- run
  */
 
 func main() {
-	cmd.Execute()
+	cobra.EnableCommandSorting = false
+
+	rootCmd := cmd.RootCmd
+	runCmd := cmd.RunCmd
+	tmCmd := tm.RootCmd
+	tmCmd.AddCommand(
+		tm.GenValidatorCmd,
+		tm.InitFilesCmd,
+		tm.ProbeUpnpCmd,
+		tm.LiteCmd,
+		tm.ReplayCmd,
+		tm.ReplayConsoleCmd,
+		tm.ResetAllCmd,
+		tm.ResetPrivValidatorCmd,
+		tm.ShowValidatorCmd,
+		tm.TestnetFilesCmd,
+		tm.ShowNodeIDCmd,
+		tm.GenNodeKeyCmd,
+		tm.VersionCmd,
+	)
+
+	cli.PrepareBaseCmd(runCmd, "TM", os.ExpandEnv(filepath.Join("$HOME", cfg.DefaultTendermintDir)))
+	cli.PrepareBaseCmd(tmCmd, "TM", os.ExpandEnv(filepath.Join("$HOME", cfg.DefaultTendermintDir)))
+
+	rootCmd.AddCommand(runCmd)
+	rootCmd.AddCommand(tmCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
