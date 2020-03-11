279ca1fabed23bfd90b1b1bf289cddd37705a3a6
Jaehong Ahn <monsieur.ahn@gmail.com>
2020-03-11 17:01:47 +0900

    Supplement migration code

--- amo/migrate.go
+++ amo/migrate.go
@@ -1,14 +1,20 @@
 package amo
 
+import (
+	"fmt"
+)
+
+const Migration = "ProtocolMigration"
+
 func (app *AMOApp) migrateTo(protocolVersion uint64, changes []string, operation func() error) {
 	// condition for migration
 	if app.state.Height != app.config.UpgradeProtocolHeight {
 		return
 	}
 
-	app.logger.Debug("Migrating to %s", protocolVersion)
+	app.logger.Info(Migration+" - BEGIN", "ProtocolVersion", protocolVersion)
 	for i, change := range changes {
-		app.logger.Debug("%d. %s", i+1, change)
+		app.logger.Info(Migration, fmt.Sprintf("change %d", i+1), change)
 	}
 
 	err := operation()
@@ -16,7 +22,7 @@ func (app *AMOApp) migrateTo(protocolVersion uint64, changes []string, operation
 		panic(err)
 	}
 
-	app.logger.Debug("Successfully migrated to %s", protocolVersion)
+	app.logger.Info(Migration+" - DONE", "ProtocolVersion", protocolVersion)
 }
 
 func (app *AMOApp) MigrateToX() {
@@ -28,3 +34,50 @@ func (app *AMOApp) MigrateToX() {
 
 	app.migrateTo(protocolVersion, changes, func() error { return nil })
 }
+
+/* sample code for migration
+
+func (app *AMOApp) MigrateTo5() {
+	protocolVersion := uint64(5)
+	changes := []string{
+		"shorten store key 'balance:' -> 'bal:'",
+		"set key-value with new prefix",
+		"remove exisiting key-value with old prefix",
+	}
+
+	app.migrateTo(protocolVersion, changes, func() error {
+		// ignore when merkle tree doesn't have available versions
+		_, err := app.store.GetLatestVersion()
+		if err != nil {
+			return nil
+		}
+
+		imt, err := app.store.GetImmutableTree(true)
+		if err != nil {
+			return err
+		}
+
+		beforeKeyPrefix := []byte("balance:")
+		afterKeyPrefix := []byte("bal:")
+
+		imt.IterateRangeInclusive(beforeKeyPrefix, nil, true, func(key, value []byte, version int64) bool {
+			if !bytes.HasPrefix(key, beforeKeyPrefix) {
+				return false
+			}
+
+			beforeKey := key
+			afterKey := append(afterKeyPrefix, key[len(beforeKeyPrefix):]...)
+
+			app.logger.Debug(Migration, "store:set", fmt.Sprintf("%x", afterKey))
+			app.store.Set(afterKey, value)
+			app.logger.Debug(Migration, "store:remove", fmt.Sprintf("%x", beforeKey))
+			app.store.Remove(beforeKey)
+
+			return false
+		})
+
+		return nil
+	})
+}
+
+*/
