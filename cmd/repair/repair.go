f7a937c08f425c6461f640032a850b88f414a815
Yeon-Hyeong Yang <yh.yang@dcgraph.com>
2020-04-23 15:16:16 +0900

    clean up

--- cmd/repair/repair.go
+++ cmd/repair/repair.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"bytes"
-	//"encoding/hex"
-	//"encoding/json"
+	"encoding/binary"
+	"encoding/hex"
+	"errors"
 	"fmt"
 	"os"
-	//"strings"
-	"errors"
+	"strings"
 
 	"github.com/tendermint/iavl"
 	tmstate "github.com/tendermint/tendermint/state"
@@ -16,7 +16,6 @@ import (
 	tmdb "github.com/tendermint/tm-db"
 
 	"github.com/amolabs/amoabci/amo"
-	//amostore "github.com/amolabs/amoabci/amo/store"
 )
 
 const (
@@ -85,16 +84,6 @@ func repair(amoRoot string, doFix bool) {
 
 	display(amoMt, amoState, tmBlockStoreState, tmState)
 
-	/*
-		responses, err := tmstate.LoadABCIResponses(sdb, amoState.LastHeight)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		b, err := json.MarshalIndent(responses, "", "  ")
-		fmt.Println("abci responses for block", amoState.LastHeight, string(b))
-	*/
-
 	//// repair
 
 	fmt.Println("Repair TM state from block store...")
@@ -107,7 +96,7 @@ func repair(amoRoot string, doFix bool) {
 	}
 	for matchStateAndBlock(tmState, nblk) == false {
 		// rewind to find the matching state
-		nblk = tmBlockStore.LoadBlock(tmState.LastBlockHeight + 1)
+		nblk = tmBlockStore.LoadBlock(tmState.LastBlockHeight)
 		if nblk == nil {
 			fmt.Println("Unable to get rewind target block")
 			return
@@ -172,6 +161,42 @@ func repair(amoRoot string, doFix bool) {
 	amoMt.LoadVersionForOverwriting(amoState.MerkleVersion)
 	tmBlockStoreState.Save(bsdb)
 	tmstate.SaveState(sdb, tmState)
+
+	// cleaning up tx index
+	indexDB, err := tmdb.NewGoLevelDB("index", amoRoot+"/data")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer indexDB.Close()
+
+	// If validator set has not changed, then these dbs have no change either.
+	// indexDelegator = tmdb.NewPrefixDB(indexDB, []byte("delegator")
+	// indexValidator = tmdb.NewPrefixDB(indexDB, []byte("validator")
+	// indexEffStake =  tmdb.NewPrefixDB(indexDB, []byte("effstake")
+	indexBlockTx := tmdb.NewPrefixDB(indexDB, []byte("blocktx"))
+	indexTxBlock := tmdb.NewPrefixDB(indexDB, []byte("txblock"))
+	defer indexBlockTx.Close()
+	defer indexTxBlock.Close()
+
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(amoState.LastHeight+1))
+	iter, err := indexBlockTx.Iterator(b, nil)
+	for ; iter.Valid(); iter.Next() {
+		fmt.Println("cleaning height", binary.BigEndian.Uint64(iter.Key()))
+		indexBlockTx.DeleteSync(iter.Key())
+	}
+
+	iter, err = indexTxBlock.Iterator(nil, nil)
+	defer iter.Close()
+	for ; iter.Valid(); iter.Next() {
+		txHash := hex.EncodeToString(iter.Key())
+		h := int64(binary.BigEndian.Uint64(iter.Value()))
+		if h > amoState.LastHeight {
+			fmt.Println("cleaning tx", strings.ToUpper(txHash))
+			indexTxBlock.DeleteSync(iter.Key())
+		}
+	}
 }
 
 func matchStateAndBlock(tmState tmstate.State, blk *tmtypes.Block) bool {
@@ -255,173 +280,3 @@ func display(mt *iavl.MutableTree, amoState amo.State, tmBsj tmstore.BlockStoreS
 	fmt.Printf("  .AppHash         = %X\n", tmState.AppHash)
 
 }
-
-/*
-func repair(amoRoot string) {
-	fmt.Println("Reparing data root:", amoRoot)
-
-	fn := amoRoot + "/data/state.json"
-	amoStateFile, err := os.OpenFile(fn, os.O_RDONLY, 0)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer amoStateFile.Close()
-
-	amoState := amo.State{}
-	err = amoState.LoadFrom(amoStateFile)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// tm dbs
-	bsdb, err := tmdb.NewGoLevelDB("blockstore", amoRoot+"/data")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer bsdb.Close()
-	sdb, err := tmdb.NewGoLevelDB("state", amoRoot+"/data")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer sdb.Close()
-
-	tmBlockStoreState := tmstore.LoadBlockStoreStateJSON(bsdb)
-	tmBlockStore := tmstore.NewBlockStore(bsdb)
-	tmState := tmstate.LoadState(sdb)
-
-	// targets
-	tmTargetHeight := tmState.LastBlockHeight
-
-	fmt.Printf("Reset amoStore")
-	err = os.RemoveAll(amoRoot + "/data/merkle.db")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	err = os.RemoveAll(amoRoot + "/data/index.db")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	err = os.RemoveAll(amoRoot + "/data/incentive.db")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	err = os.RemoveAll(amoRoot + "/data/group_counter.db")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// amo dbs
-	merkleDB, err := tmdb.NewGoLevelDB("merkle", amoRoot+"/data")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer merkleDB.Close()
-
-	indexDB, err := tmdb.NewGoLevelDB("index", amoRoot+"/data")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer indexDB.Close()
-
-	incentiveDB, err := tmdb.NewGoLevelDB("incentive", amoRoot+"/data")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer incentiveDB.Close()
-
-	groupCounterDB, err := tmdb.NewGoLevelDB("group_counter", amoRoot+"/data")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer groupCounterDB.Close()
-
-	fmt.Printf(" - Done !\n")
-
-	fmt.Printf("Reset amoState")
-	amoState = amo.State{}
-	err = amoState.SaveTo(amoStateFile)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf(" - Done !\n")
-
-	fmt.Printf("Clean up tmBlockStore:\n")
-	for i := tmBlockStoreState.Height; i > tmTargetHeight; i-- {
-		b := tmBlockStore.LoadBlock(i)
-
-		fmt.Printf("  delete blockHeight = %d, blockHash = %x", b.Height, b.Hash())
-
-		err = bsdb.DeleteSync([]byte(fmt.Sprintf("BH:%s",
-			strings.ToLower(hex.EncodeToString(b.Hash())))),
-		)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		targetKey := []byte(fmt.Sprintf("P:%v:", i))
-		itr, err := bsdb.Iterator(targetKey, nil)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		defer itr.Close()
-
-		for ; itr.Valid(); itr.Next() {
-			if !bytes.HasPrefix(itr.Key(), targetKey) {
-				continue
-			}
-
-			err := bsdb.DeleteSync(itr.Key())
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-		}
-
-		err = bsdb.DeleteSync([]byte(fmt.Sprintf("H:%v", i)))
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		err = bsdb.DeleteSync([]byte(fmt.Sprintf("C:%v", i)))
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		err = bsdb.DeleteSync([]byte(fmt.Sprintf("SC:%v", i)))
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		btmp := tmBlockStore.LoadBlockByHash(b.Hash())
-		if btmp != nil {
-			fmt.Printf(" - not properly deleted %x\n", btmp.Hash())
-		}
-
-		fmt.Printf(" - Done !\n")
-	}
-
-	fmt.Printf("Fix tmBlockStoreState: Height = %d to Height = %d",
-		tmBlockStoreState.Height,
-		tmTargetHeight,
-	)
-	tmBlockStoreState.Height = tmTargetHeight
-	tmBlockStoreState.Save(bsdb)
-	fmt.Printf(" - Done !\n")
-}
-*/
