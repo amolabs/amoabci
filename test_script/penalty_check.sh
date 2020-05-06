cc3e98ca740a923b6a1228fb6b711ef38e43582c
Jaehong Ahn <monsieur.ahn@gmail.com>
2020-05-06 16:18:41 +0900

    Apply proper pass value to penalty test

--- test_script/penalty_check.sh
+++ test_script/penalty_check.sh
@@ -15,4 +15,4 @@ out=$($CLI query stake $CLIOPT ${!addr})
 echo "stake of tu1: "$out
 
 a=$(echo $out | tr -d '^@' | python -c "import sys, json; print json.load(sys.stdin)['amount']")
-if [ -z "$a" -o "$a" != "1" ]; then fail $out; fi
+if [ -z "$a" -o "$a" != "79" ]; then fail $out; fi
