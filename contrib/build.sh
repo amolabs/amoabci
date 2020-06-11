2fc674875c6ce98e061f7c0d499d488b52d92d81
Jaehong Ahn <monsieur.ahn@gmail.com>
2020-06-11 17:43:04 +0900

    Fix minor but major typo

--- contrib/build.sh
+++ contrib/build.sh
@@ -10,30 +10,30 @@ ARTIFACT=/artifact
 
 # run docker image
 docker run -d -it \
-	--name=$NAME \
-	--volume $CURRENT_PATH:$ARTIFACT \
-	--rm \
-	$IMAGE
+    --name=$NAME \
+    --volume $CURRENT_PATH:$ARTIFACT \
+    --rm \
+    $IMAGE
 
 # tools
 docker exec -it $NAME apk add bash linux-headers make gcc g++ snappy perl zlib bzip2 lz4 zstd
 
 # build rocksdb
 docker exec -it $NAME bash -c \
-	"export DEBUG_LEVEL=0 &&
+    "export DEBUG_LEVEL=0 &&
     wget https://github.com/facebook/rocksdb/archive/v$ROCKSDB_VER.tar.gz &&
-	tar -xzf v$ROCKSDB_VER.tar.gz && make -C rocksdb-$ROCKSDB_VER shared_lib &&
+    tar -xzf v$ROCKSDB_VER.tar.gz && make -C rocksdb-$ROCKSDB_VER shared_lib &&
     strip rocksdb-$ROCKSDB_VER/librocksdb.so* &&
-    mkdir -p $ARTIFACT/rocksdb
-	cp -a rocksdb-$ROCKSDB_VER/librocksdb.so* $ARTIFACT/rocksdb"
+    mkdir -p $ARTIFACT/rocksdb &&
+    cp -a rocksdb-$ROCKSDB_VER/librocksdb.so* $ARTIFACT/rocksdb"
 
 # build leveldb
 docker exec -it $NAME bash -c \
     "wget https://github.com/google/leveldb/archive/v$LEVELDB_VER.tar.gz &&
-	tar -xzf v$LEVELDB_VER.tar.gz && make -C leveldb-$LEVELDB_VER&&
-    strip leveldb-$LEVELDB_VER/out-shared/libleveldb.so* &
-    mkdir -p $ARTIFACT/leveldb
-	cp -a leveldb-$LEVELDB_VER/out-shared/libleveldb.so* $ARTIFACT/leveldb"
+    tar -xzf v$LEVELDB_VER.tar.gz && make -C leveldb-$LEVELDB_VER &&
+    strip leveldb-$LEVELDB_VER/out-shared/libleveldb.so* &&
+    mkdir -p $ARTIFACT/leveldb &&
+    cp -a leveldb-$LEVELDB_VER/out-shared/libleveldb.so* $ARTIFACT/leveldb"
 
 # stop docker container
 docker stop $NAME
