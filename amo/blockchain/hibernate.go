82c8fda5146c446c17931f318c60db3a4e2982d0
Yeon-Hyeong Yang <lbird94@gmail.com>
2020-06-30 16:03:35 +0900

    Add MissRun management and hibernation control

---
+++ amo/blockchain/hibernate.go
@@ -0,0 +1,137 @@
+package blockchain
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+
+	"github.com/tendermint/tendermint/crypto"
+	tmdb "github.com/tendermint/tm-db"
+
+	"github.com/amolabs/amoabci/amo/store"
+	"github.com/amolabs/amoabci/amo/types"
+)
+
+type MissRuns struct {
+	store              *store.Store
+	runDB              tmdb.DB
+	height             int64
+	hibernateThreshold int64
+	hibernatePeriod    int64
+}
+
+func makeRunKey(val crypto.Address, start int64) []byte {
+	buf := make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, uint64(start))
+
+	return append(val, buf...)
+}
+
+func (m MissRuns) UpdateMissRuns(height int64, vals []crypto.Address) error {
+	batch := m.runDB.NewBatch()
+	defer batch.Close()
+
+	unfinishedRuns := [][]byte{}
+	itr, err := m.runDB.Iterator(nil, nil)
+	if err != nil {
+		return err
+	}
+	defer itr.Close()
+	// guard for rewind
+	for ; itr.Valid(); itr.Next() {
+		k := itr.Key()
+		b := k[crypto.AddressSize:]
+		runStart := int64(binary.BigEndian.Uint64(b))
+		b = itr.Value()
+		runLen := int64(binary.BigEndian.Uint64(b))
+
+		if runStart > height {
+			batch.Delete(k)
+		} else if runStart+runLen > height || runLen == 0 {
+			// run cropped, and made to be an unfinished run
+			unfinishedRuns = append(unfinishedRuns, k)
+		}
+	}
+
+	// process input missing validators
+	for _, v := range vals {
+		hit := false
+		for i, r := range unfinishedRuns {
+			runVal := r[:crypto.AddressSize]
+			b := r[crypto.AddressSize:]
+			runStart := int64(binary.BigEndian.Uint64(b))
+			if bytes.Equal(runVal, v) {
+				// continue unfinished run
+				hit = true
+				// TODO: do Set() only when there is a change of value
+				buf := make([]byte, 8)
+				binary.BigEndian.PutUint64(buf, uint64(0))
+				batch.Set(r, buf)
+				if height+1-runStart >= m.hibernateThreshold {
+					hib := types.Hibernate{
+						Start: height,
+						End:   height + m.hibernatePeriod,
+					}
+					m.store.SetHibernate(runVal, &hib)
+				}
+				l := len(unfinishedRuns)
+				unfinishedRuns[i] = unfinishedRuns[l-1]
+				unfinishedRuns[l-1] = nil
+				unfinishedRuns = unfinishedRuns[:l-1]
+				break
+			}
+		}
+		// not in the previous set of missing validators
+		// start a new run
+		if !hit {
+			k := makeRunKey(v, height)
+			buf := make([]byte, 8)
+			binary.BigEndian.PutUint64(buf, uint64(0))
+			batch.Set(k, buf)
+		}
+	}
+
+	// close unfinished runs
+	for _, r := range unfinishedRuns {
+		b := r[crypto.AddressSize:]
+		runStart := int64(binary.BigEndian.Uint64(b))
+		buf := make([]byte, 8)
+		binary.BigEndian.PutUint64(buf, uint64(height-runStart))
+		batch.Set(r, buf)
+	}
+
+	batch.Write()
+
+	return nil
+}
+
+func (m MissRuns) getLastMissRun(val crypto.Address) (start, length int64) {
+	start = 0
+	length = 0
+
+	b := make(crypto.Address, crypto.AddressSize+8)
+	copy(b, val)
+	end := append(b[:crypto.AddressSize],
+		[]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}...)
+	itr, err := m.runDB.ReverseIterator(val, end)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer itr.Close()
+
+	for ; itr.Valid(); itr.Next() {
+		k := itr.Key()
+		runVal := k[:crypto.AddressSize]
+		if !bytes.Equal(runVal, val) {
+			return
+		}
+		b := k[crypto.AddressSize:]
+		start = int64(binary.BigEndian.Uint64(b))
+		b = itr.Value()
+		length = int64(binary.BigEndian.Uint64(b))
+		return
+	}
+
+	return
+}
---
+++ amo/blockchain/hibernate_test.go
@@ -0,0 +1,134 @@
+package blockchain
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tendermint/tendermint/crypto"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+	tmdb "github.com/tendermint/tm-db"
+
+	"github.com/amolabs/amoabci/amo/store"
+	"github.com/amolabs/amoabci/amo/types"
+	"github.com/amolabs/amoabci/crypto/p256"
+)
+
+func makeAccAddr(seed string) crypto.Address {
+	return p256.GenPrivKeyFromSecret([]byte(seed)).PubKey().Address()
+}
+
+func makeValAddr(seed string) crypto.Address {
+	priKey := ed25519.GenPrivKeyFromSecret([]byte(seed))
+	pubKey := priKey.PubKey().(ed25519.PubKeyEd25519)
+	return pubKey.Address()
+}
+
+func makeHibernate(start, end int64) *types.Hibernate {
+	hib := types.Hibernate{
+		Start: start,
+		End:   end,
+	}
+	return &hib
+}
+
+func TestHibernate(t *testing.T) {
+	s, err := store.NewStore(nil, 1, tmdb.NewMemDB(), tmdb.NewMemDB(), tmdb.NewMemDB())
+	assert.NoError(t, err)
+	assert.NotNil(t, s)
+
+	mr := MissRuns{
+		store:              s,
+		runDB:              tmdb.NewMemDB(),
+		hibernateThreshold: 10,
+		hibernatePeriod:    100,
+	}
+
+	var start, length int64
+
+	val1 := makeValAddr("val1")
+	val2 := makeValAddr("val2")
+	mvals := []crypto.Address{}
+	mval10 := []crypto.Address{val1}
+	mval12 := []crypto.Address{val1, val2}
+	mval02 := []crypto.Address{val2}
+
+	hib := s.GetHibernate(val1, false)
+	assert.Nil(t, hib)
+
+	err = mr.UpdateMissRuns(3, mvals)
+	assert.NoError(t, err)
+	start, length = mr.getLastMissRun(val1)
+	assert.Equal(t, int64(0), start)
+	assert.Equal(t, int64(0), length)
+
+	err = mr.UpdateMissRuns(4, mval10)
+	assert.NoError(t, err)
+	err = mr.UpdateMissRuns(5, mval12)
+	assert.NoError(t, err)
+	err = mr.UpdateMissRuns(6, mval12)
+	assert.NoError(t, err)
+	err = mr.UpdateMissRuns(7, mval02)
+	assert.NoError(t, err)
+	start, length = mr.getLastMissRun(val1)
+	assert.Equal(t, int64(4), start)
+	assert.Equal(t, int64(3), length)
+	start, length = mr.getLastMissRun(val2)
+	assert.Equal(t, int64(5), start)
+	assert.Equal(t, int64(0), length) // unfinished run has length zero
+
+	// alternative history from height 6
+	err = mr.UpdateMissRuns(6, mvals)
+	assert.NoError(t, err)
+	err = mr.UpdateMissRuns(7, mval02)
+	assert.NoError(t, err)
+	start, length = mr.getLastMissRun(val1)
+	assert.Equal(t, int64(4), start)
+	assert.Equal(t, int64(2), length)
+	start, length = mr.getLastMissRun(val2)
+	assert.Equal(t, int64(7), start)
+	assert.Equal(t, int64(0), length) // unfinished run has length zero
+
+	// history gap does not break unfinished run
+	err = mr.UpdateMissRuns(9, mval02)
+	assert.NoError(t, err)
+	start, length = mr.getLastMissRun(val2)
+	assert.Equal(t, int64(7), start)
+	assert.Equal(t, int64(0), length) // unfinished run has length zero
+
+	// hibernate test
+	// val came back before hibernation
+	for h := int64(10); h < 19; h++ {
+		err = mr.UpdateMissRuns(h, mval10)
+		assert.NoError(t, err)
+	}
+	start, length = mr.getLastMissRun(val1)
+	assert.Equal(t, int64(10), start)
+	assert.Equal(t, int64(0), length)
+	err = mr.UpdateMissRuns(20, mvals) // empty missing validators
+	start, length = mr.getLastMissRun(val1)
+	assert.Equal(t, int64(10), start)
+	assert.Equal(t, int64(10), length)
+	hib = s.GetHibernate(val1, false)
+	assert.Nil(t, hib)
+
+	// hibernate test
+	// val goes to hibernate at height 19
+	for h := int64(10); h < 20; h++ {
+		err = mr.UpdateMissRuns(h, mval10)
+		assert.NoError(t, err)
+	}
+	start, length = mr.getLastMissRun(val1)
+	assert.Equal(t, int64(10), start)
+	assert.Equal(t, int64(0), length)
+	hib = s.GetHibernate(val1, false)
+	assert.NotNil(t, hib)
+	assert.Equal(t, int64(19), hib.Start)
+	assert.Equal(t, int64(119), hib.End)
+	// one more height to close the run:
+	// Since val1 is set to hibernate at height 19, it will not be included in
+	// the missed validators at height 20.
+	err = mr.UpdateMissRuns(20, mvals)
+	start, length = mr.getLastMissRun(val1)
+	assert.Equal(t, int64(10), start)
+	assert.Equal(t, int64(10), length)
+}
