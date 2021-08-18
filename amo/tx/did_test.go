d615b0e487ab7c03821b618615d0601fbde03417
Yeon-Hyeong Yang <lbird94@gmail.com>
2021-08-18 18:22:58 +0900

    DID format check

--- amo/tx/did_test.go
+++ amo/tx/did_test.go
@@ -93,43 +93,73 @@ func TestTxClaimV6(t *testing.T) {
 	entry := s.GetDIDEntry("myid", false)
 	assert.Nil(t, entry)
 
-	// first claim
+	// tx check error
 	payload, _ := json.Marshal(ClaimParam{
-		Target:   "myid",
+		// invalid AMO DID format
+		Target: "did:amo:Z0EAD5B53B11DFE78EC8CF131D7960F097D48D70",
+		// invalid DID document (no "id")
 		Document: []byte(`{}`),
 	})
 	t1 := makeTestTxV6("claim", "sender", payload)
 	rc, info := t1.Check()
+	assert.Equal(t, code.TxCodeBadParam, rc)
+	assert.Contains(t, info, "invalid byte")
+
+	payload, _ = json.Marshal(ClaimParam{
+		// valid AMO DID format
+		Target: "did:amo:70EAD5B53B11DFE78EC8CF131D7960F097D48D70",
+		// invalid DID document (no "id")
+		Document: []byte(`{}`),
+	})
+	t1 = makeTestTxV6("claim", "sender", payload)
+	rc, info = t1.Check()
+	assert.Equal(t, code.TxCodeBadParam, rc)
+	assert.Equal(t, "mismatching did", info)
+
+	// first claim
+	myid := "did:amo:70EAD5B53B11DFE78EC8CF131D7960F097D48D70"
+	mydoc := Document{
+		Id: myid,
+	}
+	mydocJson, _ := json.Marshal(mydoc)
+	payload, _ = json.Marshal(ClaimParamV6{
+		Target:   myid,
+		Document: mydoc,
+	})
+	t1 = makeTestTxV6("claim", "sender", payload)
+	rc, info = t1.Check()
 	assert.Equal(t, code.TxCodeOK, rc)
 	assert.Equal(t, "ok", info)
 	rc, _, _ = t1.Execute(s)
 	assert.Equal(t, code.TxCodeOK, rc)
 	assert.Equal(t, "ok", info)
 
-	entry = s.GetDIDEntry("myid", false)
+	entry = s.GetDIDEntry(myid, false)
 	assert.NotNil(t, entry)
-	assert.Nil(t, entry.Owner)
-	assert.True(t, bytes.Equal([]byte(`{}`), entry.Document))
+	assert.Nil(t, entry.Owner) // in protocl v6, entry.Owner becomes obsolete
+	assert.True(t, bytes.Equal(mydocJson, entry.Document))
 
 	// update claim
-	payload, _ = json.Marshal(ClaimParam{
-		Target:   "myid",
-		Document: []byte(`{"haha": "hoho"}`),
+	mydoc.Controller = "did:amo:0687D766FF0563B86BFF078B7F560AFC070C81AD"
+	mydocJson, _ = json.Marshal(mydoc)
+	payload, _ = json.Marshal(ClaimParamV6{
+		Target:   myid,
+		Document: mydoc,
 	})
 	t2 := makeTestTxV6("claim", "sender", payload)
 	rc, _, _ = t2.Execute(s)
 	assert.Equal(t, code.TxCodeOK, rc)
 	assert.Equal(t, "ok", info)
 
-	entry = s.GetDIDEntry("myid", false)
+	entry = s.GetDIDEntry(myid, false)
 	assert.NotNil(t, entry)
-	assert.Nil(t, entry.Owner)
+	assert.Nil(t, entry.Owner) // in protocl v6, entry.Owner becomes obsolete
 	// XXX note that retrieved document is a compact representation
-	assert.True(t, bytes.Equal([]byte(`{"haha":"hoho"}`), entry.Document))
+	assert.True(t, bytes.Equal(mydocJson, entry.Document))
 
 	// dsmiss
 	payload, _ = json.Marshal(DismissParam{
-		Target: "myid",
+		Target: myid,
 	})
 	t3 := makeTestTxV6("dismiss", "sender", payload)
 	rc, info = t3.Check()
@@ -139,6 +169,6 @@ func TestTxClaimV6(t *testing.T) {
 	assert.Equal(t, code.TxCodeOK, rc)
 	assert.Equal(t, "ok", info)
 
-	entry = s.GetDIDEntry("myid", false)
+	entry = s.GetDIDEntry(myid, false)
 	assert.Nil(t, entry)
 }
--- amo/tx/did_v6.go
+++ amo/tx/did_v6.go
@@ -1,9 +1,11 @@
 package tx
 
 import (
-	//"bytes"
 	//"encoding/binary"
 	//"encoding/json"
+	"encoding/hex"
+	"encoding/json"
+	"strings"
 
 	abci "github.com/tendermint/tendermint/abci/types"
 
@@ -14,6 +16,39 @@ import (
 
 //// claim
 
+type ClaimParamV6 struct {
+	Target   string   `json:"target"`
+	Document Document `json:"document"`
+}
+
+type Document struct {
+	Context            string `json:"@context,omitempty"`
+	Id                 string `json:"id"`
+	Controller         string `json:"controller,omitempty"`
+	VerificationMethod struct {
+		Id           string `json:"id"`
+		Type         string `json:"type"`
+		Controller   string `json:"controller,omitempty"`
+		PublicKeyJwk struct {
+			Kty string `json:"kty"`
+			Crv string `json:"crv"`
+			X   string `json:"x"`
+			Y   string `json:"y"`
+		} `json:"publicKeyJwk"`
+	} `json:"verificationMethod"`
+	Authentication  string `json:"authentication"`
+	AssertionMethod string `json:"assertionMethod,omitempty"`
+}
+
+func parseClaimParamV6(raw []byte) (ClaimParamV6, error) {
+	var param ClaimParamV6
+	err := json.Unmarshal(raw, &param)
+	if err != nil {
+		return param, err
+	}
+	return param, nil
+}
+
 type TxClaimV6 struct {
 	TxBase
 	Param ClaimParam `json:"-"`
@@ -22,22 +57,39 @@ type TxClaimV6 struct {
 var _ Tx = &TxClaimV6{}
 
 func (t *TxClaimV6) Check() (uint32, string) {
-	_, err := parseClaimParam(t.getPayload())
+	param, err := parseClaimParamV6(t.getPayload())
 	if err != nil {
 		return code.TxCodeBadParam, err.Error()
+	}
+
+	ss := strings.Split(param.Target, ":")
+	if len(ss) != 3 || ss[0] != "did" || ss[1] != "amo" || len(ss[2]) != 40 {
+		return code.TxCodeBadParam, "invalid target did"
+	}
+	_, err = hex.DecodeString(ss[2])
+	if err != nil {
+		return code.TxCodeBadParam, err.Error()
+	}
+
+	if param.Target != param.Document.Id {
+		return code.TxCodeBadParam, "mismatching did"
 	}
 
 	return code.TxCodeOK, "ok"
 }
 
 func (t *TxClaimV6) Execute(store *store.Store) (uint32, string, []abci.Event) {
-	txParam, err := parseClaimParam(t.getPayload())
+	txParam, err := parseClaimParamV6(t.getPayload())
 	if err != nil {
 		return code.TxCodeBadParam, err.Error(), nil
 	}
 
+	b, err := json.Marshal(txParam.Document)
+	if err != nil {
+		return code.TxCodeBadParam, err.Error(), nil
+	}
 	entry := &types.DIDEntry{
-		Document: txParam.Document,
+		Document: b,
 	}
 	store.SetDIDEntry(txParam.Target, entry)
 
@@ -72,4 +124,3 @@ func (t *TxDismissV6) Execute(store *store.Store) (uint32, string, []abci.Event)
 
 	return code.TxCodeOK, "ok", []abci.Event{}
 }
-
